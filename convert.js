e47efbca4d9ea24108b72ee906c8f7c32f5d2c03
Thughes37 <143362238+Thughes37@users.noreply.github.com>
2023-09-11 19:33:23 +0000

    toFixed(2) to toFixed(0)

--- convert.js
+++ convert.js
@@ -1,13 +1,63 @@
-window.addEventListener("DOMContentLoaded", domLoaded);
+document.addEventListener("DOMContentLoaded", domLoaded);
 
 function domLoaded() {
-   // TODO: Complete the function
+    const cInput = document.getElementById("cInput");
+    const fInput = document.getElementById("fInput");
+    const convertButton = document.getElementById("convertButton");
+    const errorMessage = document.getElementById("errorMessage");
+    const weatherImage = document.getElementById("weatherImage");
+
+    // Input event handlers to clear opposing input field
+    cInput.addEventListener("input", () => {
+        fInput.value = "";
+    });
+
+    fInput.addEventListener("input", () => {
+        cInput.value = "";
+    });
+
+   // Click event handler for the Convert button
+convertButton.addEventListener("click", () => {
+    const celsiusValue = parseFloat(cInput.value);
+    const fahrenheitValue = parseFloat(fInput.value);
+
+    if (!isNaN(celsiusValue) && isNaN(fahrenheitValue)) {
+        // Convert Celsius to Fahrenheit
+        const convertedFahrenheit = convertCtoF(celsiusValue);
+        fInput.value = convertedFahrenheit.toFixed(0); // Display with 2 decimal places
+        showImage(convertedFahrenheit, weatherImage);
+        errorMessage.innerHTML = "";
+    } else if (isNaN(celsiusValue) && !isNaN(fahrenheitValue)) {
+        // Convert Fahrenheit to Celsius
+        const convertedCelsius = convertFtoC(fahrenheitValue);
+        cInput.value = convertedCelsius.toFixed(0); // Display with 2 decimal places
+        showImage(fahrenheitValue, weatherImage);
+        errorMessage.innerHTML = "";
+    } else {
+        // Handle bad input
+        errorMessage.innerHTML = `${cInput.value} is not a number`;
+        weatherImage.src = "";
+    }
+});
+
+
 }
 
+// Function to convert Celsius to Fahrenheit
 function convertCtoF(degreesCelsius) {
-   // TODO: Complete the function
+    return (degreesCelsius * 9 / 5 + 32);
 }
 
+// Function to convert Fahrenheit to Celsius
 function convertFtoC(degreesFahrenheit) {
-   // TODO: Complete the function
+    return ((degreesFahrenheit - 32) * 5 / 9);
+}
+function showImage(tempInF, imageElement) {
+    if (tempInF < 32) {
+        imageElement.src = "cold.png";
+    } else if (tempInF >= 32 && tempInF <= 50) {
+        imageElement.src = "cool.png";
+    } else {
+        imageElement.src = "warm.png";
+    }
 }
